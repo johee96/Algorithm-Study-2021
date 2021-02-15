928b8e783eb07161281ea58ad1413fc486883d3f
johee96 <whgml1@gmail.com>
2021-02-15 18:10:55 +0900

    2021.02.15 프린터

---
+++ src/programmers/level2/프린터.java
@@ -0,0 +1,37 @@
+package programmers.level2;
+
+import java.util.*;
+
+public class 프린터 {
+    public static void main(String[] args) {
+
+    }
+
+    //todo 우선순위 큐 내부적으로 정렬방법 찾기
+    class 프린터_Solution {
+        public int solution(int[] priorities, int location) {
+            int answer = 1;
+
+            PriorityQueue priority = new PriorityQueue<>(Collections.reverseOrder());
+
+            for (int task : priorities) {
+                priority.add(task);
+//                System.out.println(priority);
+            }
+
+            while (!priority.isEmpty()) {
+                for (int i = 0; i < priorities.length; i++) {
+                    if (priorities[i] == (int) priority.peek()) {
+                        if (i == location)
+                            return answer;
+                        priority.poll();
+                        answer++;
+                    }
+                }
+            }
+
+            return answer;
+        }
+
+    }
+}
